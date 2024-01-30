eeaf6634b75556c30fc72dea594549fc41c2e31d
frank campos <frankcampos85@gmail.com>
2024-01-29 23:42:23 -0600

    Now the postcard are rendering into index.js, also I changed the getPost and getMyPost

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from 'react';
 import { getPosts } from '../api/PostData';
 import PostCard from '../components/Postcard';
