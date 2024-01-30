5b5be04e36dff4904c29f070817c1e854bd61dd9
Jay Lhomme <jhgatohagire@gmail.com>
2024-01-29 20:39:30 -0600

    api

--- pages/my-posts.js
+++ pages/my-posts.js
@@ -1,8 +1,9 @@
-
+import { getPosts } from '../.next/api/PostData';
 import { useAuth } from '../utils/context/authContext';
 
 function MyPosts() {
   const { user } = useAuth();
+  console.warn(getPosts);
   return (
     <div
       className="text-center d-flex flex-column justify-content-center align-content-center"
@@ -16,6 +17,7 @@ function MyPosts() {
       <h1>{user.fbUser.displayName}&apos;s Posts! </h1>
 
     </div>
+
   );
 }
 
